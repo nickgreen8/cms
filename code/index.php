035eb6f9cc75da18ed52c9ca05a5f344e4e6412d
Nick Green <nick@nickgreenweb.co.uk>
2014-12-26 19:04:25 +0000

    Log class created and integration tested

--- code/index.php
+++ code/index.php
@@ -1,12 +1,22 @@
 <?php
 namespace Website;
 
-use Components\HTML\Paragraph;
+use Components\HTML\Paragraph,
+	Utils\Log;
+
+error_reporting(E_ALL);
+date_default_timezone_set('Europe/London');
 
 require_once __DIR__ . '/autoload.php';
 $loader = Autoload::getInstance();
+Log::init(__DIR__ . '/library/Logs/');
 
-error_reporting(E_ALL);
+Log::fatal('Fatal log check');
+Log::warn('Warn log check');
+Log::notice('Notice log check');
+Log::info('Info log check');
+Log::debug('Debug log check');
+Log::error('Error log check');
 
 	$p = new Paragraph('test p', 'Hello World!');
 	echo $p->toHtml();
